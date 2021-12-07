00fe9e44e4da4372212d2b6c2af33aeb276f5616
Daniil Demidko <demidko@farpost.com>
2021-12-08 01:26:09 +1000

    xmake build script updated

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,8 @@
 add_rules("mode.release")
-add_requires("catch2") -- see https://xmake.io/#/package/remote_package
 
 target("main")
-  set_kind("binary")
-  set_languages("c++20")
+  set_kind("binary") -- see https://xmake.io/#/manual/project_target?id=set-target-kind
+  set_languages("c++0") -- see https://xmake.io/#/manual/project_target?id=targetset_languages
   set_warnings("all", "error")
   add_files("src/main/*.cpp")
 
@@ -14,7 +13,7 @@ target("test")
   add_files("src/test/*.cpp")
   add_includedirs("src/main")
   add_deps("main")
-  add_packages("catch2")
+  add_packages("catch2") -- see https://xmake.io/#/package/remote_package
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
