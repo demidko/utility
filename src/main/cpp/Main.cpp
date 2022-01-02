ae71f2c85403a622db8dca5d63fd7710c74b67f7
Daniil Demidko <demidko@farpost.com>
2022-01-03 00:00:13 +1000

    msvc fixes

--- src/main/cpp/Main.cpp
+++ src/main/cpp/Main.cpp
@@ -7,10 +7,10 @@
 int main(int argc, char **argv) {
   using namespace std;
   using namespace std::filesystem;
-  spdlog::info("{}", string(current_path()));
-  spdlog::info("{}", string(path(argv[0]).parent_path()));
+  spdlog::info("{}", current_path().string());
+  spdlog::info("{}", path(argv[0]).parent_path().string());
   auto resourcesDirectory = current_path() / path(argv[0]).parent_path();
-  spdlog::info("{}", string(resourcesDirectory));
+  spdlog::info("{}", resourcesDirectory.string());
   auto resourceFile = std::ifstream(resourcesDirectory / "Example.txt");
   for (std::string line; getline(resourceFile, line);) {
     spdlog::info("{}", line);
